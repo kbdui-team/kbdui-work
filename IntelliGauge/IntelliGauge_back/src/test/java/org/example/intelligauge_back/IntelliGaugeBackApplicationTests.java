c90f8754a2fc4cd875d7222979c681475e1f5395
kbdui <2915504390@qq.com>
2025-07-09 18:05:00 +0800

    新建后端springboot模块

---
+++ IntelliGauge/IntelliGauge_back/src/test/java/org/example/intelligauge_back/IntelliGaugeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.intelligauge_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliGaugeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
