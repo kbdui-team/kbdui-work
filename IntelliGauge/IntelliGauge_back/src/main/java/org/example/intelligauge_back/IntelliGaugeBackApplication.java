0df9a3af859a31b6a1f8e3176c3c78f31bd9508a
kbdui <2915504390@qq.com>
2025-07-09 18:05:00 +0800

    新建后端springboot模块

---
+++ IntelliGauge/IntelliGauge_back/src/main/java/org/example/intelligauge_back/IntelliGaugeBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.intelligauge_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliGaugeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliGaugeBackApplication.class, args);
+	}
+
+}
---
+++ IntelliGauge/IntelliGauge_back/src/main/java/org/example/intelligauge_back/test.java
@@ -0,0 +1,4 @@
+package org.example.intelligauge_back;
+
+public class test {
+}
