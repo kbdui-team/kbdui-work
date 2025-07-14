680150cb4665d0bb09d8fa57cdbd95e44f3d2103
kbdui <2915504390@qq.com>
2025-07-14 21:55:53 +0800

    后端模型接口尝试

---
+++ IntelliGauge/IntelliGauge_back/src/main/java/common/service/ChatResponseSyncDataService.java
@@ -0,0 +1,24 @@
+package common.service;
+
+import com.hugai.chatsdk.common.entity.ChatSdkStorageResponse;
+import com.hugai.chatsdk.common.entity.account.ChatSdkAccount;
+import com.hugai.chatsdk.common.entity.session.RecordData;
+import com.hugai.common.modules.entity.session.model.SessionRecordModel;
+
+import java.util.List;
+
+/**
+ * @author WuHao
+ * @since 2023/12/27 8:52
+ */
+public interface ChatResponseSyncDataService {
+
+    /**
+     * 同步持久化对话响应数据
+     *
+     * @param response
+     * @param list
+     */
+    List<SessionRecordModel> syncChatResponse(ChatSdkAccount requestParam, ChatSdkStorageResponse response, List<RecordData> list);
+
+}
